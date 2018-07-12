cef35c7b4f79ee6110fdd953bd67aea7a690fd94
Nate Book <nmbook@gmail.com>
2018-07-11 22:58:17 -0400

    [RoleReqs] Automatically update posted lists; style updates
    - use double quotes everywhere
    - add setting auto_post_list to automatically update all
      posted lists on current server when counts change or roles change
    - .req postlist -> .req post_list (alias added)
    - .req addrole, .req remrole, .req post_list, and .reqset now
      require bot role moderator or guild permission manage_guild
      (guild permission administrator is included by this).

--- rolereqs/rolereqs.py
+++ rolereqs/rolereqs.py
@@ -14,7 +14,8 @@ class RoleRequests:
         default_guild = {
                 "roles": [],
                 "max_requestable": 3,
-                "request_channel": 0
+                "request_channel": 0,
+                "auto_post_list": False,
         }
 
         default_channel = {
@@ -26,7 +27,7 @@ class RoleRequests:
         self.config.register_guild(**default_guild)
         self.config.register_channel(**default_channel)
     
-    @commands.group(aliases=['iam', 'req'])
+    @commands.group(aliases=["iam", "req"])
     @commands.guild_only()
     async def request(self, ctx):#, *, raw_text):
         """Requests access to a role."""
@@ -41,9 +42,9 @@ class RoleRequests:
 
         await ctx.send(msg)
     
-    @request.command()
+    @request.command(aliases=["post_list"])
     @commands.guild_only()
-    @checks.mod_or_permissions(administrator=True)
+    @checks.mod_or_permissions(manage_guild=True)
     async def postlist(self, ctx, channel : discord.TextChannel):
         """Lists the roles that can be requested and posts them permanently to the specified channel."""
         msg = await self._get_role_list_message(ctx)
@@ -56,29 +57,14 @@ class RoleRequests:
             return
 
         post_id = await self.config.channel(channel).role_info_post()
-        updated = False
-        if post_id > 0:
-            try:
-                post = await channel.get_message(post_id)
-                updated = True
-                if msg != post.content:
-                    await post.edit(content=msg)
-                else:
-                    updated = None
-            except discord.NotFound:
-                await ctx.send('Warning: A role information post was already posted in that channel but since deleted.')
-                post = await channel.send(msg)
-        else:
-            post = await channel.send(msg)
+        result = await self._post_list(channel, post_id, msg)
 
-        await self.config.channel(channel).role_info_post.set(post.id)
-
-        if updated is None:
-            await ctx.send('No update needed. Update it later with `{}request postlist #{}`'.format(ctx.prefix[0], channel))
-        elif updated:
-            await ctx.send('Message updated. Update it later with `{}request postlist #{}`'.format(ctx.prefix[0], channel))
+        if result is None:
+            await ctx.send("No update needed. Update it later with `{}request postlist #{}`".format(ctx.prefix[0], channel))
+        elif result:
+            await ctx.send("Message updated. Update it later with `{}request postlist #{}`".format(ctx.prefix[0], channel))
         else:
-            await ctx.send('Message posted to channel. Update it later with `{}request postlist #{}`'.format(ctx.prefix[0], channel))
+            await ctx.send("Message posted to channel. Update it later with `{}request postlist #{}`".format(ctx.prefix[0], channel))
 
     @request.command()
     @commands.guild_only()
@@ -106,16 +92,18 @@ class RoleRequests:
             if role.id in role_subset:
                 count += 1
                 if count >= max_requestable:
-                    cpl = ''
+                    cpl = ""
                     if count != 1:
-                        cpl = 's'
+                        cpl = "s"
                     await ctx.send(error("You already have {} role{} that can be requested (max: {}). You may ask a moderator or you may remove a role with with `{}request rem NAME`.".format(count, cpl, max_requestable, ctx.prefix[0])))
                     return
 
         await ctx.author.add_roles(role_to_add)
-        await ctx.send('Added {} to your roles.'.format(self._get_role_styled(role_to_add, show_stats=True)))
+        if await self.config.guild(ctx.guild).auto_post_list():
+            await self._auto_post_list(ctx)
+        await ctx.send("Added {} to your roles.".format(self._get_role_styled(role_to_add, show_stats=True)))
 
-    @request.command(aliases=['remove'])
+    @request.command(aliases=["remove"])
     @commands.guild_only()
     async def rem(self, ctx, *, role_name):
         """Takes a requestable role."""
@@ -138,9 +126,11 @@ class RoleRequests:
             return
 
         await ctx.author.remove_roles(role_to_add)
-        await ctx.send('Removed {} from your roles.'.format(self._get_role_styled(role_to_add, show_stats=True)))
+        if await self.config.guild(ctx.guild).auto_post_list():
+            await self._auto_post_list(ctx)
+        await ctx.send("Removed {} from your roles.".format(self._get_role_styled(role_to_add, show_stats=True)))
 
-    @request.command(aliases=['clr'])
+    @request.command(aliases=["clr"])
     @commands.guild_only()
     async def clear(self, ctx):
         """Clears all requestable roles."""
@@ -150,16 +140,18 @@ class RoleRequests:
         role_count = len(role_objs)
         if role_count > 0:
             await ctx.author.remove_roles(*role_objs)
+            if await self.config.guild(ctx.guild).auto_post_list():
+                await self._auto_post_list(ctx)
             if role_count > 1:
-                await ctx.send('Removed {} of your roles.'.format(role_count))
+                await ctx.send("Removed {} of your roles.".format(role_count))
             else:
-                await ctx.send('Removed {} from your roles.'.format(self._get_role_styled(role_objs[0], show_stats=True)))
+                await ctx.send("Removed {} from your roles.".format(self._get_role_styled(role_objs[0], show_stats=True)))
         else:
             await ctx.send(error("You do not have any requestable roles to remove."))
 
     @request.command()
     @commands.guild_only()
-    @checks.mod_or_permissions(administrator=True)
+    @checks.mod_or_permissions(manage_guild=True)
     async def addrole(self, ctx, *, role_name):
         """Adds a role to be requestable."""
         # find matches
@@ -173,12 +165,13 @@ class RoleRequests:
                 return
 
             role_subset.append(role_to_add.id)
-
+            if await self.config.guild(ctx.guild).auto_post_list():
+                await self._auto_post_list(ctx)
             await ctx.send(info("Added {} to requestable roles list.".format(self._get_role_styled(role_to_add, show_stats=True))))
 
-    @request.command(aliases=['removerole'])
+    @request.command(aliases=["removerole"])
     @commands.guild_only()
-    @checks.mod_or_permissions(administrator=True)
+    @checks.mod_or_permissions(manage_guild=True)
     async def remrole(self, ctx, *, role_name):
         """Removes a role from being requestable."""
         # find matches
@@ -192,12 +185,13 @@ class RoleRequests:
                 return
 
             role_subset.remove(role_to_add.id)
-
+            if await self.config.guild(ctx.guild).auto_post_list():
+                await self._auto_post_list(ctx)
             await ctx.send(info("Removed {} from requestable roles list.".format(self._get_role_styled(role_to_add, show_stats=True))))
 
-    @request.command(aliases=['massapplyrole', 'massapply'])
+    @request.command(aliases=["massapplyrole", "massapply"])
     @commands.guild_only()
-    @checks.mod_or_permissions(administrator=True)
+    @checks.mod_or_permissions(manage_guild=True)
     async def massadd(self, ctx, limit : int = 1000, channel : discord.TextChannel = None, *, role_name):
         """Adds roles to all users who have participated in a channel within the last X messages."""
         # requestable list
@@ -224,7 +218,7 @@ class RoleRequests:
             async for message in channel.history(limit=limit):
                 if not message.author.bot and \
                    not message.author == ctx.message.author and \
-                   hasattr(message.author, 'roles') and \
+                   hasattr(message.author, "roles") and \
                    not message.author in accounts and \
                    not role_to_add in message.author.roles:
                     accounts.append(message.author)
@@ -234,10 +228,14 @@ class RoleRequests:
                 await ctx.send(error("No users have participated in the last {} messages in {}.".format(n, channel.mention)))
             elif len(accounts) == 1:
                 await accounts[0].add_roles(role_to_add)
+                if await self.config.guild(ctx.guild).auto_post_list():
+                    await self._auto_post_list(ctx)
                 await ctx.send(info("Added {} to {}'s roles (only participant in the last {} messages in {}).".format(self._get_role_styled(role_to_add, show_stats=True), accounts[0], n, channel.mention)))
             else:
                 for account in accounts:
                     await account.add_roles(role_to_add)
+                if await self.config.guild(ctx.guild).auto_post_list():
+                    await self._auto_post_list(ctx)
                 await ctx.send(info("Added {} to {} users' roles (participants in the last {} messages in {}).".format(self._get_role_styled(role_to_add, show_stats=True), len(accounts), n, channel.mention)))
 
     def _get_role_styled(self, role_obj, *, show_stats=False):
@@ -247,14 +245,15 @@ class RoleRequests:
             role_txt = role_obj.mention
 
         if show_stats:
-            color = ''
+            color = ""
             if role_obj.color != discord.Color.default() and role_obj.mentionable:
-                color = '{}; '.format(role_obj.color)
+                color = "{}; ".format(role_obj.color)
             return "{} ({}{})".format(role_txt, color, len(role_obj.members))
         else:
             return role_txt
 
     async def _get_role_list_message(self, ctx, channel : discord.TextChannel = None):
+        """Generates a postable role list."""
         n = 0
         msg = ""
         rolereqs = await self.config.guild(ctx.guild).roles()
@@ -270,7 +269,7 @@ class RoleRequests:
             for role_id in rolereqs:
                 if role_obj.id == role_id:
                     if (n % 5) == 0:
-                        msg += '\n'
+                        msg += "\n"
                     n += 1
                     msg += "{}  ".format(self._get_role_styled(role_obj, show_stats=True))
                     break
@@ -281,6 +280,7 @@ class RoleRequests:
             return "__***REQUESTABLE ROLES ON THIS SERVER***__ ({number} roles){roles}\n\nModify your roles{in_chan} with:\n`{prefix}request add NAME`\n`{prefix}request rem NAME`".format(number=n, roles=msg, prefix=ctx.prefix[0], in_chan=in_chan)
 
     async def _find_role(self, ctx, role_name, *, role_subset=None):
+        """Finds a role by text name using loose matching. Strips "@" symbols, ignores case, and accepts role pings and partial text matches."""
         if role_subset is None:
             # just get all roles on server...
             role_subset = [role.id for role in ctx.guild.role_hierarchy]
@@ -304,15 +304,15 @@ class RoleRequests:
         if role_to_add is None:
             for role_obj in sorted(ctx.guild.role_hierarchy, reverse=True):
                 if not role_obj.id in role_subset:
-                    # role isn't in "requestable subset", skip this result
+                    # role isn"t in "requestable subset", skip this result
                     continue
 
-                if role_name.lower().strip('<>@ ') == str(role_obj).lower().strip('<>@ '):
+                if role_name.lower().strip("<>@ ") == str(role_obj).lower().strip("<>@ "):
                     # found exact
                     role_to_add_results = [role_obj]
                     break
 
-                if role_name.lower().strip('<>@ ') in str(role_obj).lower().strip('<>@ '):
+                if role_name.lower().strip("<>@ ") in str(role_obj).lower().strip("<>@ "):
                     # found
                     role_to_add_results.append(role_obj)
 
@@ -323,16 +323,52 @@ class RoleRequests:
                 role_to_add = role_to_add_results[0]
                 found_on_guild = True
             else:
-                role_to_add_result_str = ', '.join(['`{}`'.format(x) for x in role_to_add_results])
+                role_to_add_result_str = ", ".join(["`{}`".format(x) for x in role_to_add_results])
                 await ctx.send(error("Text matches multiple possible roles: {}".format(role_to_add_result_str)))
                 return None
 
         return role_to_add
  
+    async def _post_list(self, channel, post_id, msg):
+        """Posts a role list or updates an existing one in a channel."""
+        updated = False
+        if post_id > 0:
+            try:
+                post = await channel.get_message(post_id)
+                updated = True
+                if msg != post.content:
+                    await post.edit(content=msg)
+                else:
+                    updated = None
+            except discord.NotFound:
+                #await ctx.send("Warning: A role information post was already posted in that channel but since deleted.")
+                post = await channel.send(msg)
+        else:
+            post = await channel.send(msg)
+
+        await self.config.channel(channel).role_info_post.set(post.id)
+
+        return updated
+
+    async def _auto_post_list(self, ctx):
+        """Automatically updates all already-posted lists after updating user roles or role counters."""
+        msg = await self._get_role_list_message(ctx)
+        channels = await self.config.all_channels()
+        for channel_id, channel_data in channels.items():
+            if "role_info_post" in channel_data and channel_data["role_info_post"] > 0:
+                channel = self.bot.get_channel(channel_id)
+
+                if channel is None:
+                    continue
+
+                if channel.guild.id != ctx.guild.id:
+                    continue
+
+                await self._post_list(channel, channel_data["role_info_post"], msg)
 
     @commands.group()
     @commands.guild_only()
-    @checks.mod_or_permissions(administrator=True)
+    @checks.mod_or_permissions(manage_guild=True)
     async def reqset(self, ctx):
         """Adjust [p]request command settings."""
         if ctx.invoked_subcommand is None:
@@ -340,7 +376,7 @@ class RoleRequests:
 
     @reqset.command(aliases=["req_channel", "channel"])
     @commands.guild_only()
-    @checks.mod_or_permissions(administrator=True)
+    @checks.mod_or_permissions(manage_guild=True)
     async def request_channel(self, ctx, channel : discord.TextChannel = None):
         """Where `[p]request list` commands say to use the `[p]request` command."""
         if channel is None:
@@ -352,6 +388,8 @@ class RoleRequests:
             channel_id = channel.id
 
         await self.config.guild(ctx.guild).request_channel.set(channel_id)
+        if await self.config.guild(ctx.guild).auto_post_list():
+            await self._auto_post_list(ctx)
         if channel_id != 0:
             await ctx.send(info("Set where to suggest using the `[p]request` commands in `[p]request list` to {}.".format(channel.mention)))
         else:
@@ -359,7 +397,7 @@ class RoleRequests:
 
     @reqset.command(aliases=["max_req", "max"])
     @commands.guild_only()
-    @checks.mod_or_permissions(administrator=True)
+    @checks.mod_or_permissions(manage_guild=True)
     async def max_requestable(self, ctx, count : int):
         """Maximum number of roles that users can request."""
         if count < 0:
@@ -367,3 +405,14 @@ class RoleRequests:
 
         await self.config.guild(ctx.guild).max_requestable.set(count)
         await ctx.send(info("Set maximum number of requestable roles per user to {}.".format(count)))
+
+    @reqset.command(aliases=["auto_postlist"])
+    @commands.guild_only()
+    @checks.mod_or_permissions(manage_guild=True)
+    async def auto_post_list(self, ctx, val : bool):
+        """Whether to automatically update existing post_list posts when roles or counts change."""
+        await self.config.guild(ctx.guild).auto_post_list.set(val)
+        if val:
+            await ctx.send(info("Will automatically update post_list posts."))
+        else:
+            await ctx.send(info("Will not automatically update post_list posts."))
