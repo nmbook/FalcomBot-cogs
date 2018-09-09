65a9ba7be2bcc943935a12b7e251a10fec8b95dd
Nate Book <nmbook@gmail.com>
2018-09-09 15:20:43 -0400

    [Wikia] Update imports for Red V3b20 support

--- wikia/wikia.py
+++ wikia/wikia.py
@@ -1,7 +1,5 @@
 import discord
-from discord.ext import commands
-from redbot.core import Config
-from redbot.core import checks
+from redbot.core import commands, Config, checks
 from redbot.core.utils.chat_formatting import escape
 import aiohttp
 import asyncio
